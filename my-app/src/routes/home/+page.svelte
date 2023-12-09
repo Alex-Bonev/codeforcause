8e6792c8b7a3160318dab60357a26b268aedc4a5
Alex-Bonev <improbablyalex@gmail.com>
2023-12-09 09:34:06 -0800

    starter base

---
+++ my-app/src/routes/home/+page.svelte
@@ -0,0 +1,15 @@
+
+
+<div class="h-screen grid grid-cols-2 grid-rows-1">
+  <div class="bg-background h-screen flex flex-col gap-8 p-8">
+    <div class="bg-slate-300 h-64 rounded-2xl">
+      past pdfs
+    </div>
+    <button class="bg-slate-300">
+      upload pdfs
+    </button>
+  </div>
+  <div class="bg-primary h-screen">
+    bbb
+  </div>
+</div>
